74493d8926d2a6de8516f8414aba06201e748855
Ajamison2011 <Anthonyjamison94@gmail.com>
2022-07-29 11:36:39 -0400

    Concat Example

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { noop, Observable } from 'rxjs';
+import { concat, interval, noop, Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
 
@@ -13,6 +13,17 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const source1$ = interval(1000);
+
+    const source2$ = of(4,5,6);
+
+    const source3$ = of(7,8,9);
+
+    const results$ = concat(source1$,source2$,source3$)
+    .subscribe(
+      res => console.log(res)
+      );
     
   }
 
