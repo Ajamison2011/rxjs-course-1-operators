c20f979be3e1032fa0c138a129cdd91e059bd51a
Ajamison2011 <Anthonyjamison94@gmail.com>
2022-08-01 14:18:33 -0400

    custom debug operator

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -16,6 +16,7 @@ import {
 import {merge, fromEvent, Observable, concat} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import { createHttpObservable } from '../common/util';
+import { debug, RxJsLoggingLevel, setRxJsLoggingLevel } from '../common/debug';
 
 
 @Component({
@@ -40,20 +41,26 @@ export class CourseComponent implements OnInit, AfterViewInit {
     ngOnInit() {
 
         this.courseId = this.route.snapshot.params['id'];
-        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`);
+        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`)
+        .pipe(
+            debug(RxJsLoggingLevel.INFO, "course value ")
+        );
+        setRxJsLoggingLevel(RxJsLoggingLevel.TRACE)
     }
 
     ngAfterViewInit() {
-       const searchLessons$ = fromEvent<any>(this.input.nativeElement,'keyup').pipe(
+    
+        
+        this.lessons$ =  fromEvent<any>(this.input.nativeElement,'keyup')
+        .pipe(
             map(event => event.target.value),
+            startWith(''),
+            debug(RxJsLoggingLevel.TRACE, "search"),
             debounceTime(400),
             distinctUntilChanged(),
-            switchMap(search => this.loadLessons(search))
+            switchMap(search => this.loadLessons(search)),
+            debug(RxJsLoggingLevel.DEBUG, "Lessons value "),
         );
-        
-        const initialLessons$ = this.loadLessons();
-        
-        this.lessons$ =  concat(initialLessons$,searchLessons$);
         
 
     }
